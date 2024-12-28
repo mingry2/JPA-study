cf5b6e1e29bdad078dfc4d134085b330086e823f
minkyung kim <minry20415@naver.com>
2024-12-28 11:08:07 +0900

    ✨: 회원 컨트롤러 구현 및 Thymeleaf 뷰 템플릿 사용(+부트스트랩)

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,70 @@
+package jpabook.jpashop.controller;
+
+import jakarta.annotation.PostConstruct;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+
+    }
+
+    @PostMapping("/new")
+    public String create(@Validated MemberForm form,
+                         BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+    @PostConstruct
+    public void init() {
+        Member member1 = new Member();
+        member1.setName("John Doe");
+        member1.setAddress(new Address("London", "London", "Korea"));
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("민경");
+        member2.setAddress(new Address("Korea", "Korea", "Korea"));
+        memberService.join(member2);
+
+    }
+}
